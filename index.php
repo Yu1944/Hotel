c3ca99536a14f49b3c7a9def04687a210ef05470
ksmink1 <6022170@mborijnland.nl>
2023-12-05 12:46:20 +0100

    index + header
    - header toegevoegd
    - wat css toegevoegd
    - db conn (oop)
    -script.js (empty)

---
+++ index.php
@@ -0,0 +1,36 @@
+
+<?php
+
+require 'header.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hotel</title>
+</head>
+<body>
+  <div class="slideshows">
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="images/img_nature_wide.jpg" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="images/img_snow_wide.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="asset/img_mountains_wide.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+</div>
+</body>
+</html>
