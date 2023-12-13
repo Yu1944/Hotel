4ecca5ff8a9c8a8d6bff7638f8552a5ca3e01e23
ksmink1 <6022170@mborijnland.nl>
2023-12-13 08:51:06 +0100

    problem fixing
    index slideshow automatisch en correct werkend(idk hoe?)
    een register page voor acc.

--- header.php
+++ header.php
@@ -9,17 +9,18 @@ require 'db.php';
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <script src="script.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
-<body></body>
+<body>
+    <br>
     <header>
         <nav>
             <ul>
                 <li><a href="index.php">Home</a></li>
-                <li><a href="kamer_overicht.php">kamer_overicht</a></li>
+                <li><a href="room.php">kamer_overicht</a></li>
                 <li><a href="">Login</a></li>
             </ul>
         </nav>
 </header>
+</body>
 </html>
