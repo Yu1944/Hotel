d73bb59ee0d3c75ccd788b6a68b7294260f6c8c7
ksmink1 <6022170@mborijnland.nl>
2023-12-13 12:54:21 +0100

    header fix

--- header.php
+++ header.php
@@ -17,14 +17,7 @@ require 'db.php';
             <ul>
                 <li><a href="index.php">Home</a></li>
                 <li><a href="room.php">kamer_overicht</a></li>
-                <?php
-            if (isset($_SESSION['user_id'])) {
-             
-                echo '<li><a href="logout.php">Logout</a></li>';
-            } else {
-                echo '<li><a href="login.php">Login</a></li>';
-            }
-            ?>
+                <li><a href="login.php">Login</a></li>
             </ul>
         </nav>
     </header>
