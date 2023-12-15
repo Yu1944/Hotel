2b1052cef63f9a1431bd105d9773d7ad97a7c4f2
ksmink1 <97698002+ksmink1@users.noreply.github.com>
2023-12-15 13:32:14 +0100

    github wilt niet  de file pushen dus hier

---
+++ privacy.php
@@ -0,0 +1,41 @@
+<?php
+include 'header.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+
+
+  <h2> Ontvangers</h2>
+
+  <p>Wij delen uw persoonsgegevens niet met bedrijven, organisaties en individuen buiten hotel mbo, behalve in een van de volgende omstandigheden.</p>
+
+  <h3>(Direct) Marketing</h3>
+
+  <p>Om aanbiedingen te doen die u kunnen interesseren, hebben wij contact met alle Van der Valk Hotels en Restaurants wereldwijd. Zij hebben van ons toestemming gekregen om informatie en aanbiedingen te plaatsen op onze webpagina's. Als u een bezoek brengt aan onze website en op de informatie pagina's en aanbiedingen van deze hotels klikt, kunnen deze hotels op internet dan ook informatie verzamelen, zoals uw domeinnaam en uw IP-adres.</p>
+
+  <h3>Met uw toestemming</h3>
+
+  <p>Met uw toestemming kunnen wij uw persoonsgegevens aan andere partijen doorgeven. De toestemming geldt alleen indien duidelijk is waar u uw toestemming voor geeft en wat de gevolgen daarvan zijn.</p>
+
+  <h3>Voor externe verwerking</h3>
+
+  <p>We verstrekken persoonsgegevens aan onze partners zodat ze gegevens voor ons kunnen verwerken, op basis van onze instructies en in overeenstemming met ons privacy beleid en andere passende vertrouwelijkheids- en beveiligingsmaatregelen. Onder onze partners verstaan wij onder meer onze IT-leveranciers en beheerder van het CRM-systeem.</p>
+
+  <h3>Om juridische redenen</h3>
+
+  <p>We delen persoonsgegevens als we van mening zijn dat openbaarmaking van de gegevens noodzakelijk is om te voldoen aan de van toepassing zijnde wet- en regelgeving, wettelijke procedures of verzoeken van overheidsinstanties.</p>
+
+  <h3>Wettelijke verplichting</h3>
+
+  <p>Indien een wettelijke verplichting dit van ons vereist, zullen wij uw persoonsgegevens verstrekken. Zo is het bijvoorbeeld mogelijk dat de politie in het kader van fraudeonderzoek gegevens bij ons opvraagt. Een ander voorbeeld is dat de belastinginspecteur op grond van artikel 47 van de Algemene wet inzake rijksbelastingen alle gegevens kan opeisen die nodig zijn om belasting te heffen. Van der Valk maakt afspraken met de ontvangers van uw persoonsgegevens om ervoor te zorgen dat de persoonsgegevens vertrouwelijk worden behandeld en worden beveiligd.</p>
+
+
+  
+<script src="script.js"></script>
+</body>
+</html> 
+<?php include 'footer.php';?>
+</html>
